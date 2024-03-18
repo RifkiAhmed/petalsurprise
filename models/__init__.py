403f6024b7b3f5e4e1dff3ef35dc10ae82db411e
ahmed <rifkiahmed.91@gmail.com>
2024-03-18 23:35:52 +0000

    Add authentication object initialization

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+"""
+Initializes the authentication and database session for the APP
+"""
+from models.auth import Auth
 from models.engine.db import DB
+
+AUTH = Auth()
 storage = DB()
 storage._session
