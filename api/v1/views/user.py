6e14bd16bdb73e417773464e526609cebe53c7f5
ahmed <rifkiahmed.91@gmail.com>
2024-03-18 22:44:00 +0000

    Add /user_orders endpoint for user orders

--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -2,12 +2,40 @@
 """
 User view for the API
 """
-from flask import abort, jsonify, request, render_template
 from api.v1.views import views
-from models.auth import Auth
 from datetime import datetime, timedelta
+from flask import abort, jsonify, request, render_template
+from models import storage, AUTH
+from models.order import Order
 
-AUTH = Auth()
+
+FORMAT = "%Y-%m-%d %H:%M"
+
+
+def _orders_data(orders):
+    orders_data = []
+    for order in orders:
+        order_data = {
+            'id': order.id,
+            'created_at': datetime.strftime(
+                order.created_at, FORMAT),
+            'recipient_name': order.recipient_name,
+            'recipient_address': order.recipient_address,
+            'products_number': len(order.products),
+            'message': order.message,
+            'payment_method_type': order.payment_method_type,
+            'status': order.status,
+            'amount': int(order.amount) / 100,
+            'products': []
+        }
+        for product in order.products:
+            products_data = {
+                'name': product.name,
+                'image': product.img_path
+            }
+            order_data['products'].append(products_data)
+        orders_data.append(order_data)
+    return orders_data
 
 
 @views.route("/users", methods=["POST"])
@@ -50,13 +78,8 @@ def logout() -> None:
         abort(403)
     AUTH.destroy_session(user)
     response = jsonify({})
-    response.set_cookie(
-        "session_id",
-        "",
-        expires=datetime.now() +
-        timedelta(
-            days=-
-            1))
+    response.set_cookie("session_id", "",
+                        expires=datetime.now() + timedelta(days=-1))
     return response
 
 
@@ -73,11 +96,40 @@ def profile() -> str:
     return render_template('/profile.html', user=user, email=user.email)
 
 
-@views.route("/reset_password", methods=["PUT"])
-def update_password() -> str:
-    """ User password reset
+@views.route("/profile", methods=["PUT"])
+def update_profile() -> str:
+    """ Update user profile
     """
-    new_password = request.form.get("newPassword")
+    session_id = request.cookies.get("session_id")
+    email = request.form.get("email")
+    username = request.form.get("username")
+    current_password = request.form.get("currentPassword")
+    new_password = request.form.get("password")
+    if not session_id:
+        abort(401)
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        if not user:
+            abort(403)
+        if username:
+            storage.update(user, username=username)
+            return jsonify({"message": "Username updated"}), 200
+        if email:
+            storage.update(user, email=email)
+            return jsonify({"message": "Email updated"}), 200
+        if new_password:
+            if not AUTH.valid_login(user.email, current_password):
+                abort(401)
+            AUTH.update_password(user, new_password=new_password)
+            return jsonify({"message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
+@views.route("/user_orders", methods=["GET"])
+def user_activity() -> str:
+    """ User orders history
+    """
     session_id = request.cookies.get("session_id")
     if not session_id:
         abort(401)
@@ -85,7 +137,8 @@ def update_password() -> str:
         user = AUTH.get_user_from_session_id(session_id)
         if not user:
             abort(403)
-        AUTH.update_password(user, new_password)
-        return jsonify({"message": "Password updated"}), 200
+        orders = storage.find_all(Order, user_id=user.id)
+        orders_data = _orders_data(orders)
+        return jsonify(orders_data), 200
     except ValueError:
         abort(403)
