298abbbf65ffec2d3c71d270458d34ca11e44638
ahmed <rifkiahmed.91@gmail.com>
2024-03-24 22:52:48 +0000

    Add get_range_filter() and get_string_filter() methods

--- models/engine/db.py
+++ models/engine/db.py
@@ -5,29 +5,28 @@ DB model
 from models.basemodel import Base
 from sqlalchemy import create_engine, and_, func
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
 from os import getenv
 
 
 class DB:
-    """ DB class
+    """DB class
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """ Initialize a new DB instance
         """
         self._engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                      format(getenv('DB_USER'),
                                             getenv('DB_PWD'),
                                             getenv('DB_HOST'),
                                             getenv('DB_NAME')))
-        # Base.metadata.drop_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """ Memoized session object
         """
         Base.metadata.create_all(self._engine)
         if self.__session is None:
@@ -38,12 +37,12 @@ class DB:
         return self.__session
 
     def all(self, cls):
-        """Returns all the object of the given cls argument
+        """ Returns all the object of the given cls argument
         """
         return self._session.query(cls).all()
 
     def add(self, obj):
-        """Add a new object to the database
+        """ Add a new object to the database
         """
         if obj:
             self._session.add(obj)
@@ -51,7 +50,7 @@ class DB:
         return None
 
     def delete(self, obj):
-        """Delete the object from the database
+        """ Delete the object from the database
         """
         if obj:
             self._session.delete(obj)
@@ -59,29 +58,32 @@ class DB:
         return None
 
     def save(self):
-        """Save change to the database
+        """ Save change to the database
         """
         if self._session:
             self._session.commit()
         return None
 
-    def get_limit(self, cls, page, per_page):
-        """Returns the objects corresponding to the page of size per_page
+    def get_limit(self, cls, page, per_page, sort_by=None):
+        """ Returns the objects corresponding to the page of size per_page
         """
         start = page * per_page
         end = start + per_page
         products_count = self.count(cls)
         if end > products_count:
             end = products_count
-        products = self._session.query(cls).slice(start, end).all()
+        products = self._session.query(cls)
+        if sort_by:
+            if sort_by == 'low_to_hight':
+                products = products.order_by(getattr(cls, 'price'))
+            if sort_by == 'high_to_low':
+                products = products.order_by(getattr(cls, 'price').desc())
+        products = products.slice(start, end).all()
         return products
 
     def get_range_filter(self, cls, min_price, max_price):
-        """Returns the objects corresponding to the page of size per_page
+        """ Returns the objects within a specified price range
         """
-        # start = page * per_page
-        # end = start + per_page
-        # products_count = self.count(cls)
         if min_price and max_price:
             return self._session.query(cls).filter(and_(
                 cls.price >= min_price, cls.price <= max_price)).all()
@@ -93,11 +95,8 @@ class DB:
                 cls.price <= max_price).all()
 
     def get_string_filter(self, cls, search_name):
-        """ Returns the objects corresponding to the page of size per_page
+        """ Returns the objects whose name contains a given pattern
         """
-        # start = page * per_page
-        # end = start + per_page
-        # products_count = self.count(cls)
         return self._session.query(cls) \
             .filter(cls.name.like(f'%{search_name}%')).all()
 
@@ -116,18 +115,20 @@ class DB:
         return objs
 
     def orders_overview(self, cls):
-        """ Returns all the objects based on the given keyword argument
+        """ Returns orders stats
         """
-        stats_1 = self._session.query(cls.status, func.count(cls.id) \
-                                    .label('count_orders'), \
-                                        func.sum(cls.amount).label('total_amount')) \
-                                        .group_by(cls.status) \
-                                        .all()
-        stats_2 = self._session.query(func.date(cls.created_at).label('creation_date'),
-                                     func.count(cls.id).label('count_orders'),
-                                     func.sum(cls.amount).label('total_amount')) \
-                                        .group_by(func.date(cls.created_at)) \
-                                        .all()
+        stats_1 = self._session.query(
+            cls.status,
+            func.count(cls.id).label('count_orders'),
+            func.sum(cls.amount).label('total_amount')) \
+            .group_by(cls.status) \
+            .all()
+        stats_2 = self._session.query(
+            func.date(cls.created_at).label('creation_date'),
+            func.count(cls.id).label('count_orders'),
+            func.sum(cls.amount).label('total_amount')) \
+            .group_by(func.date(cls.created_at)) \
+            .all()
         return [stats_1, stats_2]
 
     def update(self, obj, **kwargs) -> None:
